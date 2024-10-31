1019da68ef20f3a70773616925b869896216f6cb
malik1243 <154369335+basit1215@users.noreply.github.com>
2024-11-01 03:54:05 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+
+var dates = document.getElementById("dates");
+var years = document.getElementById("years");
+var months = document.getElementById("months");
+var days = document.getElementById("days");
+dates.max = new Date().toISOString().split("T")[0];
+
+
+
+function calculateDateOfBirth(){
+    
+   var dateOfBirth = new Date(dates.value).getTime();
+   
+    var now = new Date().getTime();
+    var diff = now - dateOfBirth;
+    console.log(diff)
+    var y = Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25));
+    var m = Math.floor(diff % (1000 * 60 * 60 * 24 * 365.25) / (1000 * 60 * 60 * 24 * (365.25/12)));
+    var d = Math.floor(diff % (1000 * 60 * 60 * 24 * (365.25/12)) / (1000 * 60 * 60 * 24) );
+    years.innerHTML = y;
+    months.innerHTML = m;
+    days.innerHTML = d;
+}
